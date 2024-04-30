d550531b5925f3f2cb4652453f59b49186a6f439
Aleksa Buncic <33495263+CommandLineFox@users.noreply.github.com>
2024-04-30 16:08:29 +0200

    Jos editovanja permisija jer sam kreten (#138)
    * Ruta
    
    * Permission check
    
    * Permisije opet

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -1,44 +1,46 @@
 import { EmployeePermissionsV2 } from "./types";
 
 export const permissionMap = {
-    list_users : 0b1,
-    create_users : 0b10,
-    edit_users : 0b100,
-    deactivate_users : 0b1000,
+    list_users: 0b1,
+    create_users: 0b10,
+    edit_users: 0b100,
+    deactivate_users: 0b1000,
 
-    list_workers : 0b10000,
-    create_workers : 0b100000,
-    edit_workers : 0b1000000,
-    deactivate_workers : 0b10000000,
+    list_workers: 0b10000,
+    create_workers: 0b100000,
+    edit_workers: 0b1000000,
+    deactivate_workers: 0b10000000,
 
-    list_firms : 0b100000000,
-    create_firms : 0b1000000000,
-    edit_firms : 0b10000000000,
-    deactivate_firms : 0b100000000000,
+    list_firms: 0b100000000,
+    create_firms: 0b1000000000,
+    edit_firms: 0b10000000000,
+    deactivate_firms: 0b100000000000,
 
-    list_bank_accounts : 0b1000000000000,
-    create_bank_accounts : 0b10000000000000,
-    deactivate_bank_accounts : 0b100000000000000,
+    list_bank_accounts: 0b1000000000000,
+    create_bank_accounts: 0b10000000000000,
+    deactivate_bank_accounts: 0b100000000000000,
 
-    list_credits : 0b1000000000000000,
-    accept_redits : 0b10000000000000000,
-    deny_credits : 0b100000000000000000,
+    list_credits: 0b1000000000000000,
+    manage_credits: 0b10000000000000000,
+    accept_redits: 0b10000000000000000,
+    deny_credits: 0b1000000000000000000,
 
-    list_cards : 0b1000000000000000000,
-    activate_cards : 0b10000000000000000000,
-    deactivate_cards : 0b100000000000000000000,
-    block_cards : 0b1000000000000000000000,
+    list_cards: 0b10000000000000000000,
+    manage_cards: 0b100000000000000000000,
+    activate_cards: 0b1000000000000000000000,
+    deactivate_cards: 0b10000000000000000000000,
+    block_cards: 0b100000000000000000000000,
 
-    list_orders : 0b10000000000000000000000,
-    accept_orders : 0b100000000000000000000000,
-    deny_orders : 0b1000000000000000000000000,
+    list_orders: 0b1000000000000000000000000,
+    accept_orders: 0b10000000000000000000000000,
+    deny_orders: 0b100000000000000000000000000,
 
-    exchange_access : 0b10000000000000000000000000,
-    payment_access : 0b100000000000000000000000000,
-    action_access : 0b1000000000000000000000000000,
-    option_access : 0b10000000000000000000000000000,
-    order_access : 0b100000000000000000000000000000,
-    termin_access : 0b1000000000000000000000000000000
+    exchange_access: 0b1000000000000000000000000000,
+    payment_access: 0b10000000000000000000000000000,
+    action_access: 0b100000000000000000000000000000,
+    option_access: 0b1000000000000000000000000000000,
+    order_access: 0b10000000000000000000000000000000,
+    termin_access: 0b100000000000000000000000000000000
 }
 
 type Permission = {
@@ -106,10 +108,12 @@ export const decodePermissionsObj = (permissions: number) => {
         'deactivate_bank_accounts': false,
 
         'list_credits': false,
+        'manage_credits': false,
         'accept_redits': false,
         'deny_credits': false,
 
         'list_cards': false,
+        'manage_cards': false,
         'activate_cards': false,
         'deactivate_cards': false,
         'block_cards': false,
@@ -127,7 +131,7 @@ export const decodePermissionsObj = (permissions: number) => {
     }
     for (const [name, value] of Object.entries(permissionMap)) {
         if (permissions & value) {
-            if (Object.hasOwn(perms ,name)) {
+            if (Object.hasOwn(perms, name)) {
                 perms[name] = true
             }
         }
